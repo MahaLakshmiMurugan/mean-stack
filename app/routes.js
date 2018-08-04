eeb0c19fa7b02df58bc07bef59bb21e477ca6f14
maha <maha@pearlthoughts.com>
2018-08-04 23:05:52 +0530

    fix contact list save logic

--- app/routes.js
+++ app/routes.js
@@ -6,8 +6,43 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html');
+	app.get('/contacts', function(req, res) {
+		console.log('Getting contact list...');
+		var ContactList = require('./models/ContactList');
+		var test = ContactList.find().exec(function (err, docs) {
+			res.json(docs);
+		});
 	});
-
+	
+ 	app.post('/contacts', function(req, res) {
+		console.log('Inserting contact list...');
+		var ContactList = require('./models/ContactList');
+		new ContactList(req.body).save(function (err, docs) {
+			res.json(docs);
+		});
+	});
+ 	app.delete('/contacts/:id', function(req, res) {
+		console.log('Deleting contact...');
+		var id = req.params.id;
+		var ContactList = require('./models/ContactList');
+		ContactList.deleteOne({ _id: id }, function (err, docs) {
+			res.json(docs);
+		});
+	});
+ 	app.get('/contacts/:id', function(req, res) {
+		console.log('Getting single contact...');
+		var id = req.params.id;
+		var ContactList = require('./models/ContactList');
+		ContactList.findOne({ _id: id }, function (err, docs) {
+			res.json(docs);
+		});
+	});
+ 	app.put('/contacts/:id', function(req, res) {
+		console.log('Updating single contact...');
+		var id = req.params.id;
+		var ContactList = require('./models/ContactList');
+		ContactList.findOneAndUpdate({ _id: id }, { $set: req.body}, function (err, docs) {
+			res.json(docs);
+		});
+	});
 };
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,42 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, $http) {
 
-	$scope.tagline = 'To the moon and back!';	
+	var refresh = function () {
+		$http.get('/contacts').then(function (response) {
+			console.log(response.data);
+			$scope.contactlist = response.data;
+			$scope.contact = null;
+		});
+	};
+
+	refresh();
+
+	$scope.addContact = function () {
+		$http.post('/contacts', $scope.contact).then(function (response) {
+			console.log(response.data);
+			refresh();
+		});
+	};
+
+	$scope.remove = function (id) {
+		$http.delete('/contacts/' + id).then(function (response) {
+			refresh();
+		});
+	};
+
+	$scope.edit = function (id) {
+		$http.get('/contacts/' + id).then(function (response) {
+			$scope.contact = response.data;
+		});
+	};
+
+	$scope.update = function () {
+		$http.put('/contacts/' + $scope.contact._id, $scope.contact).then(function (response) {
+			refresh();
+		});
+	};
+
+	$scope.clear = function () {
+		$scope.contact = null;
+	}	
 
 });
